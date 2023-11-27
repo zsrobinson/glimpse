237abae2a1bdf1c64b21c56f514d1890c838a27d
Zachary Robinson <me@zsrobinson.com>
2023-11-27 14:34:57 -0500

    allow user to enter api token

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 export default function Home() {
+  const [apiToken, setApiToken] = useState<string | null>(
+    localStorage.getItem("apiToken"), // grab saved value if it exists
+  );
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <main className="m-8 flex flex-col items-start gap-4">
-      <p className="text-2xl font-semibold">Hello world!</p>
-      <Button onClick={() => alert("hi")}>Click me</Button>
+      <h1 className="text-2xl font-semibold">Todoist Weekly</h1>
+
+      {apiToken === null ? (
+        <div className="flex w-full max-w-sm items-center gap-2">
+          <Input className="font-mono" ref={inputRef} placeholder="API Token" />
+          <Button
+            onClick={() => {
+              const value = inputRef.current?.value ?? "";
+              setApiToken(value);
+              localStorage.setItem("apiToken", value); // persist value
+            }}
+          >
+            Submit
+          </Button>
+        </div>
+      ) : (
+        <>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("apiToken");
+              setApiToken(null);
+            }}
+            variant="secondary"
+          >
+            Clear API Token
+          </Button>
+          <p>weekly view thing goes here!</p>
+        </>
+      )}
     </main>
   );
 }
